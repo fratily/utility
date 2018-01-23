0966350474b7a4effebfd9e807b9edb45afe1142
kento-oka <kento.oka@kentoka.com>
2018-01-23 13:42:51 +0900

    password_*関数のクラスを追加

---
+++ src/Password.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * FratilyPHP Utility
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @author      Kento Oka <kento.oka@kentoka.com>
+ * @copyright   (c) Kento Oka
+ * @license     MIT
+ * @since       1.0.0
+ */
+namespace Fratily\Utility;
+
+/**
+ * 
+ */
+class Password{
+        
+    public static function getInfo(string $hash){
+        return password_get_info($hash);
+    }
+    
+    public static function hash(string $passwd, int $algo, array $options = []){
+        return password_hash($passwd, $algo, $options);
+    }
+    
+    public static function verify(string $passwd, string $hash){
+        return password_verify($passwd, $hash);
+    }
+    
+    public static function needsRehash(string $hash, int $algo, array $options = []){
+        return password_needs_rehash($hash, $algo, $options);
+    }
+}
