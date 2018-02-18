a6737c26a8a76de32d892b12763006b67e2d288b
kento-oka <kento.oka@kentoka.com>
2018-02-18 13:41:48 +0900

    PHPdoc追加

--- src/Hash.php
+++ src/Hash.php
@@ -14,10 +14,18 @@
 namespace Fratily\Utility;
 
 /**
- * 
+ *
  */
 class Hash{
-    
+
+    /**
+     * キーにマッチする値をすべて取得する
+     *
+     * @param   array   $root
+     * @param   string  $key
+     *
+     * @return  mixed[]
+     */
     private static function getMatchedNodes(array $root, string $key){
         $nodes  = [$root];
 
@@ -25,9 +33,9 @@ class Hash{
             if(empty($nodes)){
                 break;
             }
-            
+
             $newNodes   = [];
-            
+
             foreach($nodes as $node){
                 if(is_array($node)){
                     if($key === "*"){
@@ -42,10 +50,18 @@ class Hash{
 
             $nodes  = $newNodes;
         }
-        
+
         return $nodes;
     }
-    
+
+    /**
+     * キーにマッチする値をすべて取得する
+     *
+     * @param   array   $root
+     * @param   string  $key
+     *
+     * @return  mixed|mixed[]|null
+     */
     public static function get(array $root, string $key){
         $nodes  = self::getMatchedNodes($root, $key);
 
@@ -58,12 +74,29 @@ class Hash{
         return $nodes;
     }
 
+    /**
+     * 配列にキーにマッチする値が存在するか確認する
+     *
+     * @param   array   $root
+     * @param   string  $key
+     *
+     * @return  bool
+     */
     public static function has(array $root, string $key){
         $nodes  = self::getMatchedNodes($root, $key);
-        
+
         return !empty($nodes);
     }
 
+    /**
+     * 配列のキーに一致する所に値を挿入する
+     *
+     * @param   array   $root
+     * @param   string  $key
+     * @param   mixed   $val
+     *
+     * @return  array
+     */
     public static function set(array $root, string $key, $val){
         $origin = $root;
         $node   = &$root;
@@ -72,12 +105,12 @@ class Hash{
             if(!is_array($key) || !array_key_exists($key, $node)){
                 return $origin;
             }
-            
+
             $node   = &$node[$key];
         }
-        
+
         $node   = $val;
-        
+
         return $root;
     }
 }
