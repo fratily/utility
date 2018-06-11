0828a9837c5789a3d93a9efdf6e396c693c1d133
kento-oka <kento-oka@kentoka.com>
2018-06-11 14:44:46 +0900

    メールアドレス変わったよ

--- src/Password.php
+++ src/Password.php
@@ -6,7 +6,7 @@
  * For full copyright and license information, please see the LICENSE
  * Redistributions of files must retain the above copyright notice.
  *
- * @author      Kento Oka <kento.oka@kentoka.com>
+ * @author      Kento Oka <kento-oka@kentoka.com>
  * @copyright   (c) Kento Oka
  * @license     MIT
  * @since       1.0.0
